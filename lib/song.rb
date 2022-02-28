62d1f5288de5489adc19e7fd3c13651d9199d7d0
Read Langworthy <rlangworthy@gmail.com>
2022-02-28 11:44:53 -0800

    commit tree 982efbdc796a06abf86cfe64b22bd1f8cf429a69

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,25 @@ class Song
     song.save
   end
 
+  def self.new_from_db(row)
+    self.new(id: row[0], name: row[1], album: row[2])
+    
+  end
+
+
+  def self.all
+    DB[:conn].execute( "SELECT * FROM songs" ).map do |row|
+      self.new_from_db(row)
+    end
+    
+  end
+
+  def self.find_by_name(name)
+    DB[:conn].execute("SELECT * FROM songs WHERE name = ? LIMIT 1", name).map do |row|
+      self.new_from_db(row)
+    end.first
+    
+  end
+
+
 end
